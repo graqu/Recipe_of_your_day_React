790b35af075c70ecbbd04223c87ce1bcf04f8f04
Jacek <jacek_135@yahoo.pl>
2023-06-04 22:19:49 +0200

    react changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircleCheck, faPen, faTrashCan } from '@fortawesome/free-solid-svg-icons'
+import { faArrowDown, faArrowUp, faPen, faTrashCan } from '@fortawesome/free-solid-svg-icons'
 
 import './App.css'
 import { Toast } from 'bootstrap'
@@ -9,8 +9,8 @@ import { Toast } from 'bootstrap'
 function App() {
 	// Recipes (Ideas for dish) states
 	const [recipe, setRecipe] = useState([
-		{ id: 1, title: 'Food 1', opened: false },
-		{ id: 2, title: 'Food 2', opened: false },
+		{ id: 1, title: 'Food 1', ingrediends: 'one, alpha', description: 'sample description 1', opened: false },
+		{ id: 2, title: 'Food 2', ingrediends: 'one, beta', description: 'sample description 2', opened: false },
 	])
 	//Temp State
 	const [newRecipe, setNewRecipe] = useState('')
@@ -20,7 +20,13 @@ function App() {
 	const addRecipe = () => {
 		if (newRecipe) {
 			let num = recipe.length + 1
-			let newEntry = { id: num, title: newRecipe, opened: false }
+			let newEntry = {
+				id: num,
+				title: newRecipe,
+				ingrediends: newRecipe.ingrediends,
+				description: newRecipe.description,
+				opened: false,
+			}
 			setRecipe([...recipe, newEntry])
 			setNewRecipe('')
 		}
@@ -41,17 +47,25 @@ function App() {
 		})
 		setRecipe(newRecipe)
 	}
-	//unMark/shrink Recipe
-	const rollUpRecipe = () => {
-		//
+	//cancelUpdate
+	const cancelUpdate = () => {
+		setUpdateData('')
 	}
 	//Change Recipe
 	const changeRecipe = e => {
-		//
+		let newEntry = {
+			id: updateData.id,
+			title: e.target.value,
+			opened: updateData.opened,
+		}
+		setUpdateData(newEntry)
 	}
 	//Update Recipe
 	const updateRecipe = () => {
-		//
+		let filteredRecords = [...recipe].filter(item => item.id !== updateData.id)
+		let updatedObject = [...filteredRecords, updateData]
+		setRecipe(updatedObject)
+		setUpdateData('')
 	}
 
 	return (
@@ -61,40 +75,49 @@ function App() {
 			<h2>Recipe of your day - React App</h2>
 			<br />
 			<br />
-
 			{/* {Update Recipe} */}
-
-			<div className="row">
-				<div className="col">
-					<input className="form-control form-control-lg" />
-				</div>
-				<div className="col-auto">
-					<button className="btn btn-lg btn-success mr-20">Update</button>
-					<button className="btn btn-lg btn-warning">Cancel</button>
-				</div>
-			</div>
-			<br />
-
-			{/* {Add Recipe} */}
-
-			<div className="row">
-				<div className="col">
-					<input
-						value={newRecipe}
-						onChange={e => setNewRecipe(e.target.value)}
-						className="form-control form-control-lg"
-					/>
-				</div>
-				<div className="col-auto">
-					<button onClick={addRecipe} className="btn btn-lg btn-success">
-						Add Recipe
-					</button>
-				</div>
-			</div>
-			<br />
+			{updateData && updateData ? (
+				<>
+					<div className="row">
+						<div className="col">
+							<input
+								value={updateData && updateData.title}
+								onChange={e => changeRecipe(e)}
+								className="form-control form-control-lg"
+							/>
+						</div>
+						<div className="col-auto">
+							<button onClick={updateRecipe} className="btn btn-lg btn-success mr-20">
+								Update
+							</button>
+							<button onClick={cancelUpdate} className="btn btn-lg btn-warning">
+								Cancel
+							</button>
+						</div>
+					</div>
+					<br />
+				</>
+			) : (
+				<>
+					<div className="row">
+						<div className="col">
+							<input
+								value={newRecipe}
+								onChange={e => setNewRecipe(e.target.value)}
+								className="form-control form-control-lg"
+							/>
+						</div>
+						<div className="col-auto">
+							<button onClick={addRecipe} className="btn btn-lg btn-success">
+								Add Recipe
+							</button>
+						</div>
+					</div>
+					<br />
+				</>
+			)}
 
 			{/* {Display recipes} */}
-
 			{recipe && recipe.length ? '' : 'No Recipes on your list...'}
 			{recipe &&
 				recipe
@@ -103,23 +126,36 @@ function App() {
 						return (
 							<React.Fragment key={item.id}>
 								<div className="col itemBg">
-									<div className={item.opened ? 'extended' : ''}>
+									<div>
 										<span className="itemNumber">{index + 1}</span>
 										<span className="itemText">{item.title}</span>
 									</div>
 									<div className="iconsWrap">
-										<span title="Extended / not extended" onClick={() => markRecipe(item.id)}>
-											<FontAwesomeIcon icon={faCircleCheck} />
-										</span>
-										{item.opened ? null : (
-											<span title="Edit">
-												<FontAwesomeIcon icon={faPen} />
+										{item.opened ? (
+											<span title="Extended / not extended" onClick={() => markRecipe(item.id)}>
+												<FontAwesomeIcon icon={faArrowUp} />
+											</span>
+										) : (
+											<span title="Extended / not extended" onClick={() => markRecipe(item.id)}>
+												<FontAwesomeIcon icon={faArrowDown} />
 											</span>
 										)}
+
+										<span
+											title="Edit"
+											onClick={() => setUpdateData({ id: item.id, title: item.title, opened: item.opened })}>
+											<FontAwesomeIcon icon={faPen} />
+										</span>
 
 										<span title="Delete" onClick={() => deleteRecipe(item.id)}>
 											<FontAwesomeIcon icon={faTrashCan} />
 										</span>
+									</div>
+									<div className={item.opened ? 'extended description' : 'description'}>
+										<p>{item.ingrediends}</p>
+										<div>
+											<p>{item.description}</p>
+										</div>
 									</div>
 								</div>
 							</React.Fragment>
